b1d87ab55e40ae24bb292131ea190a7b5ffda45b
Vladislav Melanitskiy <vlad.melanitski@gmail.com>
2015-08-05 18:17:32 +0300

    Fix migrations

--- db/migrate/20140814075700_create_articles.rb
+++ db/migrate/20140814075700_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :name
       t.references :author, index: true
-
+      t.integer :articles, :status
       t.timestamps
     end
   end
--- db/migrate/20150116105737_add_status_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToArticle < ActiveRecord::Migration
-  def change
-    add_column :articles, :status, :integer
-  end
-end
