5edb01f502a1be31a3afcf974c11e6f9ac7625b2
Praffesor <kuzh@rademade.com>
2016-12-07 18:47:52 +0200

    removed mongoid menu

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   mount RademadeAdmin::Engine => '/'
   namespace :rademade_admin, path: '/' do
 
-    # Mongoid
-    admin_resources :users, :posts, :tags
     # Active record
     admin_resources :authors, :articles, :rubrics
     # Sequel
