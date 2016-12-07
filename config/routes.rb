069129e79a39c064a4ac46c5c033c2bd224e9e9f
Praffesor <kuzh@rademade.com>
2016-12-07 18:58:54 +0200

    possible favicon remove

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   mount RademadeAdmin::Engine => '/'
   namespace :rademade_admin, path: '/' do
 
+    # # Mongoid
+    # admin_resources :users, :posts, :tags
     # Active record
     admin_resources :authors, :articles, :rubrics
     # Sequel
