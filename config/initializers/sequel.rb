a8dd8270d118f8b5100260328130fa01ee38493b
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2019-06-11 12:55:17 +0300

    Add erb processing to sequel.yml

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,3 +1,5 @@
-DB = Sequel.connect(YAML.load_file("#{::Rails.root}/config/sequel.yml")[::Rails.env])
+require 'erb'
+sequel_yml = YAML.load ERB.new(File.read "#{::Rails.root}/config/sequel.yml").result
+DB = Sequel.connect(sequel_yml[::Rails.env])
 DB.extension :pagination
 Sequel::Dataset::Pagination.send(:include, Kaminari::Sequel)
