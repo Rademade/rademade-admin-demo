f29617ea7d356d24f139c31b8f1f5e100a869dcc
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2019-06-20 21:26:20 +0300

    Add bin/entrypoint.sh script back

---
+++ bin/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eu
+wait-for-it -t 300 db-mariadb:3306
+wait-for-it -t 300 db-mongo:27017
+bin/rake db:migrate
+bin/rake db:migrate:sequel
+bin/rake db:schema:dump  # update db/schema.rb
+exec bin/rails server -b 0.0.0.0
