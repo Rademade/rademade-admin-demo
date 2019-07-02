edfc5d2a08465fa7a27ae34ef18cee375a504f92
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2019-07-02 18:53:30 +0300

    entrypoint.sh: no point in updating db/schema.rb

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,7 +4,6 @@ wait-for-it -t 300 db:3306
 wait-for-it -t 300 mongo:27017
 bin/rake db:migrate
 bin/rake db:migrate:sequel
-bin/rake db:schema:dump  # update db/schema.rb
 rm -rf /docroot/*
 cp -r /app/public/* /docroot
 exec bin/rails server -b 0.0.0.0
