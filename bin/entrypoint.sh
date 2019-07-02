4924dc7c8367d82c4c369f91f214cfdab7751ef8
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2019-07-02 20:56:18 +0300

    app: use fully qualified service name

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
-wait-for-it -t 300 db:3306
-wait-for-it -t 300 mongo:27017
+wait-for-it -t 300 "$PROJECT_NAME"_db:3306
+wait-for-it -t 300 "$PROJECT_NAME"_mongo:27017
 bin/rake db:migrate
 bin/rake db:migrate:sequel
 rsync -a --delete --exclude /uploads public/ /docroot
