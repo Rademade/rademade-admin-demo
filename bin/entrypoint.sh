be90a43347fd0260eb01365c971757925b35f180
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2019-07-02 18:54:54 +0300

    Use rsync to update docroot volume
    no deleting files, no copying uploads dir

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,6 +4,5 @@ wait-for-it -t 300 db:3306
 wait-for-it -t 300 mongo:27017
 bin/rake db:migrate
 bin/rake db:migrate:sequel
-rm -rf /docroot/*
-cp -r /app/public/* /docroot
+rsync -a --delete --exclude /uploads public/ /docroot
 exec bin/rails server -b 0.0.0.0
