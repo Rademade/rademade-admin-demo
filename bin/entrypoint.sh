c1fd89df1393dd368ecd258aed4485d725ff65cb
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2019-07-17 17:41:01 +0300

    Tidy up files

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,4 +5,4 @@ wait-for-it -t 300 "$PROJECT_NAME"_mongo:27017
 bin/rake db:migrate
 bin/rake db:migrate:sequel
 rsync -a --delete --exclude /uploads public/ /docroot
-exec bin/rails server -b 0.0.0.0
+exec bin/rails server --binding 0.0.0.0
